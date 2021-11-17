6ea4ab1418db75c8eb9e394144c14bfc90187760
flaribbit <1149761294@qq.com>
2021-11-17 16:50:23 +0800

    add repo link

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,6 +5,8 @@ module.exports = {
   lang: "zh-CN",
   base: "/market-docs/",
   themeConfig: {
+    repo: "myh19980409/market-docs",
+    editLinks: true,
     nav: [
       { text: "配置指南", link: "/guide" },
       { text: "API文档", link: "/api" },
